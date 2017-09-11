63f739d66c7f71472a9f3a820983cc822a575706
夏肇真 <422645940@qq.com>
2017-09-11 15:31:00 +0800

    集成 rabbitmq

---
+++ src/main/java/com/ssm/consumer/MessageConsumer.java
@@ -0,0 +1,19 @@
+package com.ssm.consumer;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+
+public class MessageConsumer implements MessageListener {
+
+    private Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @Override
+    public void onMessage(Message message) {
+        logger.info("receive message:{}",message);
+    }
+
+}
