8f4ac082add5a3d42aacd67bed0932dd0da4b2fc
夏肇真 <422645940@qq.com>
2017-09-12 10:12:16 +0800

    集群 QM

--- src/main/java/com/ssm/unit/rabbitMq/MessageConsumer.java
+++ src/main/java/com/ssm/unit/rabbitMq/MessageConsumer.java
@@ -1,4 +1,4 @@
-package com.ssm.consumer;
+package com.ssm.unit.rabbitMq;
 
 
 import org.slf4j.Logger;
--- src/main/java/com/ssm/unit/rabbitMq/MessageProducer.java
+++ src/main/java/com/ssm/unit/rabbitMq/MessageProducer.java
@@ -1,4 +1,4 @@
-package com.ssm.producer;
+package com.ssm.unit.rabbitMq;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
